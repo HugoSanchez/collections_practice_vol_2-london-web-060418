5187ec88fff23c1da632679d8f4aac2395ebf632
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-28 09:31:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,5 +46,13 @@ def find_cool(array)
   array.select { |item| item[:temperature] == "cool" }
 end
 
-def organize_schools(array)
-end
+def organize_schools(schools)
+  locations = schools.values.flat_map(&:values).uniq
+  nyc = schools.select { |school, loc| loc[:location] == "NYC" }.keys
+  sfo = schools.select { |school, loc| loc[:location] == "SF" }.keys
++  chi = schools.select { |school, loc| loc[:location] == "Chicago" }.keys
++  sorted_schools = [nyc, sfo, chi]
++
++  locations.zip(sorted_schools).to_h
++end
++
